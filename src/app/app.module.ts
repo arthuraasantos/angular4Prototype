e76d2047f3424caa4460d6f98eedafc04eb3e37e
Arthur Augusto <arthuraasantos@hotmail.com>
2017-10-19 00:25:33 -0200

    Init Nibo Prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UiSwitchModule } from '../../node_modules/angular2-ui-switch/src'
+
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { RecurrencesComponent } from './recurrences/recurrences.component';
+import { SchedulesComponent } from './schedules/schedules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    RecurrencesComponent,
+    SchedulesComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiSwitchModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recurrences/recurrences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recurrences',
+  templateUrl: './recurrences.component.html',
+  styleUrls: ['./recurrences.component.css']
+})
+export class RecurrencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
