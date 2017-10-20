15da2ff0d3afde4a5edbfad2d16fe0ef43e4ee18
Arthur Augusto <arthuraasantos@hotmail.com>
2017-10-20 00:05:44 -0200

    Habilitar recorrencia e parcelas

--- src/app/schedules/schedules.component.ts
+++ src/app/schedules/schedules.component.ts
@@ -7,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchedulesComponent implements OnInit {
 
+  public recurrenceEnabled: boolean = false;
+  public installmentsEnabled: boolean = false;
+  billingEnabled: boolean = false;
+  invoiceEnabled: boolean = false;
+  public blockRecurrence: boolean = false;
+  public blockInstallments: boolean = false;
+
+
+
   mySchedules:any = [];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  enableRecurrence(){
+
+    if(!this.recurrenceEnabled){
+      this.recurrenceEnabled = true;
+      this.installmentsEnabled = false;
+    }
+    else {
+      this.recurrenceEnabled = false;
+    }
+  }
+
+  enableInstallments(){
+    if(!this.installmentsEnabled){
+      this.recurrenceEnabled = false;
+      this.installmentsEnabled = true;
+    }
+    else{
+      this.installmentsEnabled = false;
+    }
   }
 
 }
